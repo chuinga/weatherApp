5169fe6bc3d02ffa7ae0640309c66a1b8770c4dd
chuinga <mfproencamartins@gmail.com>
2024-08-16 16:44:20 +0100

    feat: add django view

--- meteo/views.py
+++ meteo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import geocoder as geocoder
+import requests
 
-# Create your views here.
+def temp_here():
+    endpoint = 'https://api.open-meteo.com/v1/forecast'
+
+    # Get latitude and longitude from my ip
+    location = geocoder.ip('me').latlng
+    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
+    return requests.get(api_request).json()
